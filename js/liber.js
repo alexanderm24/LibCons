63b08e6c56e3e9ecb60053bf9d31f1cdc423b1b7
alexanderm24 <113379093+alexanderm24@users.noreply.github.com>
2023-03-04 17:56:21 -0500

    update

--- js/liber.js
+++ js/liber.js
@@ -1,4 +1,4 @@
 var CETEIcean = new CETEI();
-CETEIcean.getHTML5("data/caput-2-test.xml", function (data) {
+CETEIcean.getHTML5("data/caput-1-test.xml", function (data) {
   document.getElementById("TEI").appendChild(data);
 });
