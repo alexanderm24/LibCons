505f5fdc0caa0e631399e9188377acbace447d97
alexanderm24 <113379093+alexanderm24@users.noreply.github.com>
2023-03-13 21:23:42 -0400

    update

--- js/liber.js
+++ js/liber.js
@@ -11,3 +11,17 @@ const chapter = document.getElementById("caput-1");
     }
   }
 });
+
+var CETEIcean = new CETEI();
+CETEIcean.getHTML5("data/chapter-1-test.xml", function (data) {
+
+const chapter = document.getElementById("chapter-1");
+
+  for (const n of Array.from(data.getElementsByTagName("tei-div")))
+  {
+    if(n.getAttribute("n") == 1) {
+    document.adoptNode(n);
+    chapter.appendChild(n);
+    }
+  }
+});
