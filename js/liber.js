183a39c45e51b45fb48ed59e7c07797af2aec9d9
alexanderm24 <113379093+alexanderm24@users.noreply.github.com>
2023-03-21 14:26:01 -0400

    update

--- js/liber.js
+++ js/liber.js
@@ -1,11 +1,11 @@
 var CETEIcean = new CETEI();
 CETEIcean.getHTML5("data/liber.xml", function (data) {
 
-const chapter = document.getElementById("caput-1");
+const chapter = document.getElementById("caput-2");
 
   for (const n of Array.from(data.getElementsByTagName("tei-div")))
   {
-    if(n.getAttribute("n") == 1) {
+    if(n.getAttribute("n") == 2) {
     document.adoptNode(n);
     chapter.appendChild(n);
     }
@@ -15,11 +15,11 @@ const chapter = document.getElementById("caput-1");
 var CETEIcean = new CETEI();
 CETEIcean.getHTML5("data/chapter-1-test.xml", function (data) {
 
-const chapter = document.getElementById("chapter-1");
+const chapter = document.getElementById("chapter-2");
 
   for (const n of Array.from(data.getElementsByTagName("tei-div")))
   {
-    if(n.getAttribute("n") == 1) {
+    if(n.getAttribute("n") == 2) {
     document.adoptNode(n);
     chapter.appendChild(n);
     }
