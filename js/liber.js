653c91e9301d39582b51b9eb30237dec0fbc0922
alexanderm24 <113379093+alexanderm24@users.noreply.github.com>
2023-03-21 15:29:39 -0400

    update

--- js/liber.js
+++ js/liber.js
@@ -1,3 +1,64 @@
+
+var CETEIcean = new CETEI();
+CETEIcean.getHTML5("data/liber.xml", function (data) {
+
+const chapter = document.getElementByTag("la-prologue");
+
+  for (const n of Array.from(data.getElementsByTagName("tei-div")))
+  {
+    if(n.getAttribute("n") == 0) {
+    document.adoptNode(n);
+    chapter.appendChild(n);
+    }
+  }
+});
+
+var CETEIcean = new CETEI();
+CETEIcean.getHTML5("data/book.xml", function (data) {
+
+const chapter = document.getElementById("en-prologue");
+
+  for (const n of Array.from(data.getElementsByTagName("tei-div")))
+  {
+    if(n.getAttribute("n") == 0) {
+    document.adoptNode(n);
+    chapter.appendChild(n);
+    }
+  }
+});
+
+
+
+var CETEIcean = new CETEI();
+CETEIcean.getHTML5("data/liber.xml", function (data) {
+
+const chapter = document.getElementById("caput-1");
+
+  for (const n of Array.from(data.getElementsByTagName("tei-div")))
+  {
+    if(n.getAttribute("n") == 1) {
+    document.adoptNode(n);
+    chapter.appendChild(n);
+    }
+  }
+});
+
+var CETEIcean = new CETEI();
+CETEIcean.getHTML5("data/book.xml", function (data) {
+
+const chapter = document.getElementById("chapter-1");
+
+  for (const n of Array.from(data.getElementsByTagName("tei-div")))
+  {
+    if(n.getAttribute("n") == 1) {
+    document.adoptNode(n);
+    chapter.appendChild(n);
+    }
+  }
+});
+
+
+
 var CETEIcean = new CETEI();
 CETEIcean.getHTML5("data/liber.xml", function (data) {
 
@@ -13,7 +74,7 @@ const chapter = document.getElementById("caput-2");
 });
 
 var CETEIcean = new CETEI();
-CETEIcean.getHTML5("data/chapter-1-test.xml", function (data) {
+CETEIcean.getHTML5("data/book.xml", function (data) {
 
 const chapter = document.getElementById("chapter-2");
 
